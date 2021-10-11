3a795427f7e475bc222df939103e243e7e17fd55
sumaiya722 <2021381@iub.edu.bd>
2021-10-12 02:34:15 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBY0jfh_6r_1OJWqJ11Bfi66Qbym_1pdhQ",
+    authDomain: "react-firebase-authentic-e3037.firebaseapp.com",
+    projectId: "react-firebase-authentic-e3037",
+    storageBucket: "react-firebase-authentic-e3037.appspot.com",
+    messagingSenderId: "729503953531",
+    appId: "1:729503953531:web:7cb1cca5d1359c27c528de"
+};
+export default firebaseConfig;
